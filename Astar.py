ed56a27dfcc9f29cab71ef8a4f88fba30794fc46
cvande04 <Caroline.Vanderlee@tufts.edu>
2019-10-13 01:02:09 -0400

    added helper function recurse_flip (IN PROGRESS)

--- Astar.py
+++ Astar.py
@@ -9,6 +9,7 @@ def flip(index, stack, aux):
         b = aux[i-index]
         stack.append(b)
 
+
 def check_order(stack):
     in_order = True
     for i in range(0, 5):
@@ -17,29 +18,38 @@ def check_order(stack):
 
     return in_order
 
+
 def find_first_unordered(stack):
     for i in range(0, 5):
         if (stack[i] != i+1):
             return i
 
+
+def recurse_flip(ind, stack):
+    flip(ind, stack, [])
+    if(not(check_order(stack))):
+        ### ALGORITHM FOR CALCULATING INDEX TO FLIP GOES HERE ###
+        ind = find_first_unordered(stack) + 1 # placeholder: must change
+        # recurse_flip(ind, stack)
+
+
+# Main
+
 stack = []
-aux = []
 
 print "Enter the pancake stack (5 integers from 1 to 5)."
 print "Press enter after each number. Enter the stack from bottom to top."
 
 for i in range(0, 5):
-        ele = int(input())
-        stack.append(ele)
+    ele = int(input())
+    stack.append(ele)
 
 print "Your stack is, from bottom to top:"
 print stack
 
 if(not(check_order(stack))):
     ind = find_first_unordered(stack)
-    print "First unordered index: " + str(ind)
-    print "Flipping: "
-    flip(ind, stack, aux)
+    recurse_flip(ind, stack)
 
 
 print "Goal state reached:"
