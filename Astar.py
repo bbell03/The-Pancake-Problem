3f5f7d3dc3e036246e2a4c4b7c2d0ccf406f9052
cvande04 <Caroline.Vanderlee@tufts.edu>
2019-10-15 18:13:32 -0400

    cleaned up code

--- Astar.py
+++ Astar.py
@@ -1,22 +1,30 @@
 # Caroline Vanderlee and Brandon Bell
 
+# variables needed
 stack = []
 num_flips = 0
 
+
+# helper function flip
+# flips the stack at a given index
 def flip(index, stack, aux):
     for j in range(5, index, -1):
         a = stack.pop()
         aux.append(a)
 
-    for i in range(index, 5): #
+    for i in range(index, 5):
         b = aux[i-index]
         stack.append(b)
 
+    # increment number of flips
     global num_flips
     num_flips = num_flips + 1
-    # print stack
+
     return stack
 
+
+# helper function check_order
+# checks if the stack is out of order
 def check_order(stack):
     in_order = True
     for i in range(0, 5):
@@ -25,12 +33,16 @@ def check_order(stack):
     return in_order
 
 
+# helper function find_first_unordered
+# finds the first unordered element in the stack
 def find_first_unordered(stack):
-    # print('find start')
     for i in range(0, 5):
         if (stack[i] != i+1):
             return i
 
+
+# helper function count_num_unordered
+# counts the number of elements out of order in the stack
 def count_num_unordered(stack):
     count = 0;
     for i in range(0, 5):
@@ -38,47 +50,37 @@ def count_num_unordered(stack):
             count = count + 1
     return count
 
-#where x is the index of the first unordered pancake
-#finds next consecutive element and brings it to the back of the stack
+
+# helper function next_to_end
+# finds the next element out of order in the stakc
+# and brings it to the end (here the top) of the stack
 def next_to_end(x, stack):
-    # print('next start')
     global num_flips
     aux = []
     if x == 0:
-        for i in range(5): #starting from stack top look for 1 4 elems in
+        for i in range(5):
             if stack[i] == 1:
-                #pop from 1 to end of stack
                 for k in range(5-i):
                     a = stack.pop()
-                    # print a
                     aux.append(a)
-                # print stack
-                # print aux
                 for z in range(len(aux)):
                     stack.append(aux[z])
-                # print stack
                 num_flips = num_flips + 1
+                print stack
                 return stack
     elif (not(stack[4] == stack[x-1]+1)):
-        # print('in else')
         for i in range(x, 5):
-            # print('precondition')
             if stack[i] == stack[x-1]+1:
-                # print('postcondition')
                 for k in range(5-i):
                     a = stack.pop()
-                    # print a
                     aux.append(a)
-                # print stack
-                # print aux
                 for z in range(len(aux)):
                     stack.append(aux[z])
-                # print stack
                 num_flips = num_flips + 1
+                print stack
                 return stack
     else:
         return stack
-
 
 
 # Main
@@ -97,14 +99,9 @@ print "There are " + str(num_unordered) + " elements out of order in this stack.
 
 while(not(check_order(stack))):
     ind = find_first_unordered(stack)
-    # print(ind)
-    # print stack
     stack = next_to_end(ind, stack)
-    # print stack
     ind = find_first_unordered(stack)
     stack = flip(ind, stack, [])
     print stack
 
 print "The spatula was was flipped " + str(num_flips) + " times!"
-print "Goal state reached:"
-print stack
